8e03fda49a7352121ff70da07e5eb8ba1f16925c
Thomas Pyle <TP6958@zebra.com>
2023-08-16 15:52:11 -0400

    Removes logic out to another function

--- test.c
+++ test.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+void say_hello(char* name) {
+    printf("Hello, %s!\n", name);
+}
+
 int main(int argc, char *argv[]) {
     if (argc == 1) {
-        printf("Hello, World!\n");
-    } else if (argc == 2) {
-        printf("Hello, %s!\n", argv[1]);
+        say_hello("World");
     } else {
         for (int i = 1; i < argc; i++) {
-            printf("Hello, %s!\n", argv[i]);
+            say_hello(argv[i]);
         }
     }
     return 0;
